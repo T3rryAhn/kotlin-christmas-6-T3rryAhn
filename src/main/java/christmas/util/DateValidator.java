a5df2628cf7dd6fecd5ce1b81f696eaddd9a4734
Terry <t3rryahn.dev@gmail.com>
2023-11-13 19:06:11 +0900

    feat: 날짜 입력 및 검증 기능 추가
    - DateValidator 클래스를 추가하여 날짜 입력의 유효성을 검증하는 기능 구현.
    - ValidationException 클래스를 추가하여 사용자 정의 예외 처리 구현.
    
    test: DateValidator 테스트 케이스 추가
    
    테스트 내용
    - 정상적인 날짜 입력 시 예외가 발생하지 않음을 검증.
    - 비정상적인 날짜 입력 시 예외 메시지가 기대값과 일치하는지 검증.
    - 테스트 결과: 모든 케이스 통과.
    
    docs: README.md 수정
    
    - 날짜 입력 및 검증 기능에 대한 추가사항 수정.

---
+++ src/main/java/christmas/util/DateValidator.java
@@ -0,0 +1,28 @@
+package christmas.util;
+
+public class DateValidator {
+    public static final String INVALID_DATE_MESSAGE = "유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+
+    public static void validateInput(String input) {
+        validateNumeric(input);
+        int date = Integer.parseInt(input);
+        validateDate(date);
+    }
+
+    private static void validateNumeric(String input) {
+        if (input == null) {
+            throw new ValidationException(INVALID_DATE_MESSAGE);
+        }
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new ValidationException(INVALID_DATE_MESSAGE);
+        }
+    }
+
+    private static void validateDate(int date) {
+        if (date < 1 || date > 31) {
+            throw new ValidationException(INVALID_DATE_MESSAGE);
+        }
+    }
+}
